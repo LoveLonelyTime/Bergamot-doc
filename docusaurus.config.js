08603d718ffd7f4b178e50a0b15b6d26d8ad9c1c
LoveLonelyTime <1390751361@qq.com>
2024-04-03 11:51:23 +0800

    update: baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://bergamot-doc.github.io/',
+  url: 'https://lovelonelytime.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Bergamot-doc',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
